f8ef486846010f69d87238fd1e6b1d7c2157e6d4
MatteD04 <matteo.dantonio04@gmail.com>
2024-04-23 01:48:51 +0200

    feat: add first part of main

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -5,37 +5,45 @@ export default {
 </script>
 
 <template>
-    <div class="header-menu container">
-        <div>
-            <img src="../assets/img/assets/Logo.png">
-        </div>
-        <div class="nav-menu">
-            <ul>
-                <li>ciao</li>
-                <li>ciao</li>
-                <li>ciao</li>
-            </ul>
+    <div class="background-000">
+        <div class="container header-menu">
+            <div>
+                <img src="../assets/img/assets/Logo.png">
+            </div>
+            <div class="nav-menu">
+                <ul>
+                    <li class="active">HOME</li>
+                    <li><a href="#">BLOG</a></li>
+                    <li><a href="#">EVENTS</a></li>
+                    <li><a href="#">GALLERY</a></li>
+                    <li><a href="#">ABOUT US</a></li>
+                    <li><a href="#">CONTACT US</a></li>
+                    <li><a href="#">SHOP</a></li>
+                    <li><a href="#">🔍</a></li>
+                    </ul>
+            </div>
         </div>
     </div>
 </template>
 
 <style scoped lang="scss">
-.container{
-    width: 1400px;
-    margin: 0 auto;
-}
 .header-menu{
     display: flex;
     justify-content: space-between;
+    align-items: center;
     padding: 20px 0;
 }
 .nav-menu{
     ul{
         display: flex;
         align-items: center;
-        gap: 10px;
-        color: #ffffff;
+        gap: 30px;
         list-style: none;
+        a{
+            text-decoration: none;
+            color: #ffffff;
+        }
+        
     }
 }
 </style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,43 @@
+<script>
+export default {
+    name: 'AppMain'
+}
+</script>
+
+<template>
+    <div class="main-content">
+        <div>
+            <h3 class="active">INSTRUMENTAL ROCK</h3>
+        </div>
+        <div>
+            <h1>MUSIC IN THIS VIDEO</h1>
+        </div>
+        <div>
+            <button>READ MORE</button>
+        </div>
+    </div>
+</template>
+
+<style scoped lang="scss">
+.main-content{
+    background-image: url(../assets/img/assets/cms_parallax.jpg);
+    padding: 200px 0;
+    text-align: center;
+    color: white;
+    div{
+        margin: 40px 0;
+    }
+    h1{
+        font-size: 70px;
+    }
+    button{
+        color: white;
+        background-color: transparent;
+        border: 1px solid #f2870c;
+        padding: 15px 40px;
+    }
+    button:hover{
+        background-color: #f2870c;
+    }
+}
+</style>
